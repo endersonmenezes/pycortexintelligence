0f69ad57cbe0a0ec02eb493e6479c8efc16e7dae
Enderson Menezes <endersonmenezes@outlook.com>
2020-10-13 12:28:54 -0300

    We will use kwargs in the main function, so that we can work with your updates more easily.

--- pycortexintelligence/functions.py
+++ pycortexintelligence/functions.py
@@ -115,7 +115,7 @@ def upload_local_2_cube(cubo_id, file_path, auth_endpoint, credentials,
     return execution_id, headers
 
 
-def upload_to_cortex(cubo_id, file_path, plataform_url, username, password):
+def upload_to_cortex(**kwargs):
     """
     :param cubo_id:
     :param file_path:
@@ -124,10 +124,21 @@ def upload_to_cortex(cubo_id, file_path, plataform_url, username, password):
     :param password:
     :return:
     """
-    auth_endpoint = "https://{}/service/integration-authorization-service.login".format(plataform_url)
-    credentials = {"login": str(username), "password": str(password)}
-    execution_id, headers = upload_local_2_cube(
-        cubo_id, file_path, auth_endpoint, credentials
-    )
-    response = _execution_history(execution_id, LOADMANAGER, headers)
-    return response
+    # Read Kwargs
+    cubo_id = kwargs.get('cubo_id')
+    file_path = kwargs.get('file_path')
+    plataform_url = kwargs.get('plataform_url')
+    username = kwargs.get('username')
+    password = kwargs.get('password')
+
+    # Verify Kwargs
+    if cubo_id and file_path and plataform_url and username and password:
+        auth_endpoint = "https://{}/service/integration-authorization-service.login".format(plataform_url)
+        credentials = {"login": str(username), "password": str(password)}
+        execution_id, headers = upload_local_2_cube(
+            cubo_id, file_path, auth_endpoint, credentials
+        )
+        response = _execution_history(execution_id, LOADMANAGER, headers)
+        return response
+    else:
+        raise ValueError('Error validating arguments.')
